e324b6eaf8223337b20ef16141b24550162a9f8b
Pooyan J <pooyanjavidi@gmail.com>
2023-09-27 11:33:21 +0330

    Initial Commit

---
+++ Cars HubUITests/Cars_HubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cars_HubUITestsLaunchTests.swift
+//  Cars HubUITests
+//
+//  Created by Pooyan J on 7/5/1402 AP.
+//
+
+import XCTest
+
+final class Cars_HubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
